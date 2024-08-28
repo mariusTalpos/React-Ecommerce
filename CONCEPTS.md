a9062b96693ceca8792640b32d5fc628f7f46f0d
unknown <andreitalpos@yahoo.com>
2024-08-28 12:08:48 -0400

    initial commit

---
+++ CONCEPTS.md
@@ -0,0 +1,8 @@
+## Portfolio Project
+
+The purpose of this project is to demonstrate mastery over these concepts in React
+- Routing
+- Authentication
+- State Management
+- Observer Patterns
+- Project Scaffolding
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './categories.styles.scss'
+import CategoriesContainer from './components/categories-container/categories-container.component';
+
+const App = () => {
+  
+  return (
+    <div className="App">
+      <CategoriesContainer></CategoriesContainer>
+    </div>
+  );
+}
+
+
+export default App;
